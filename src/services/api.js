eaf3d14ea14120274818eaa8c36f4a3f935929f0
Denys Borysiuk <denisborisiuk@gmail.com>
2023-02-07 18:42:42 +0200

    bugfix

--- src/services/api.js
+++ src/services/api.js
@@ -25,7 +25,7 @@ export const getMovieDetails = async (movieId, signal) => {
 
 export const searchMovie = async (query, signal) => {
   const response = await axios.get(
-    `https://api.themoviedb.org/3'/search/movie/`,
+    `https://api.themoviedb.org/3/search/movie/`,
     {
       params: {
         api_key: API_KEY,
