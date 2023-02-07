c73cfeab830e834d9b685c1329d8df8a6dd3c4e9
Denys Borysiuk <denisborisiuk@gmail.com>
2023-02-07 19:40:37 +0200

    fix url

--- src/services/api.js
+++ src/services/api.js
@@ -4,22 +4,22 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'b60c32a6f6f2663f6a35c755ff35b4cc';
 
 export const getTrendingMovies = async signal => {
-  const response = await axios.get(`${BASE_URL}/trending/movie/day`, {
-    params: {
-      api_key: API_KEY,
-    },
-    signal,
-  });
+  const response = await axios.get(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`,
+    {
+      signal,
+    }
+  );
   return response.data;
 };
 
 export const getMovieDetails = async (movieId, signal) => {
-  const response = await axios.get(`${BASE_URL}/movie/${movieId}`, {
-    params: {
-      api_key: API_KEY,
-    },
-    signal,
-  });
+  const response = await axios.get(
+    `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`,
+    {
+      signal,
+    }
+  );
   return response.data;
 };
 
@@ -34,21 +34,21 @@ export const searchMovie = async (query, signal) => {
 };
 
 export const getMovieCredits = async (movieId, signal) => {
-  const response = await axios.get(`${BASE_URL}/movie/${movieId}/credits`, {
-    params: {
-      api_key: API_KEY,
-    },
-    signal,
-  });
+  const response = await axios.get(
+    `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`,
+    {
+      signal,
+    }
+  );
   return response.data;
 };
 
 export const getMovieReviews = async (movieId, signal) => {
-  const response = await axios.get(`${BASE_URL}/movie/${movieId}/reviews`, {
-    params: {
-      api_key: API_KEY,
-    },
-    signal,
-  });
+  const response = await axios.get(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`,
+    {
+      signal,
+    }
+  );
   return response.data;
 };
